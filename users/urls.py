4078f157b481aa5f48832b6df766f4d095a4f420
samuel <mwangangimuvisi@gmail.com>
2026-05-06 13:36:47 +0300

    changes

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ router.register(r'users', UsersViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path("login/", LoginAPIView.as_view(), name="login"),
-    path("api/account/users/<uuid:uuid>/", PatchUserAPIView.as_view()),
+    path("users/<uuid:uuid>/", PatchUserAPIView.as_view()),
 
 
 ]
