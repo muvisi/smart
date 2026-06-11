91548ba15fa4203a7b26251b6c6da0dbcce61380
samuel <mwangangimuvisi@gmail.com>
2026-06-11 13:12:38 +0300

    technical

--- etims/admin.py
+++ etims/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import DebitCredit
+
+
+@admin.register(DebitCredit)
+class DebitCreditAdmin(admin.ModelAdmin):
+    list_display = (
+        "debit_credit_reference",
+        "source_pushnote_code",
+        "transaction_code",
+        "client_name",
+        "transaction_total_amount",
+        "sync_status",
+        "etims_status",
+        "created_at",
+    )
+
+    list_filter = (
+        "sync_status",
+        "etims_status",
+        "created_at",
+    )
+
+    search_fields = (
+        "debit_credit_reference",
+        "client_name",
+        "client_pin",
+        "kra_ref",
+    )
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+        "last_synced_at",
+    )
+
+    ordering = ("-created_at",)
