4e590a9a2193a37c2789b9a12644bd5c88cbc889
blairMe <felixanachoni@gmail.com>
2026-07-24 16:43:13 +0300

    Add Follow Up Report API endpoint to URLs

--- care_management/urls.py
+++ care_management/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import LouStatusReportAPIView, LouStatusReportDownloadAPIView
+from .views import FollowUpReportAPIView, LouStatusReportAPIView, LouStatusReportDownloadAPIView
 
 urlpatterns = [
     path("lou-status-report/download/<str:filename>", LouStatusReportDownloadAPIView.as_view(), name="lou-status-report-download"),
     path("lou-status-report/download/<str:filename>/", LouStatusReportDownloadAPIView.as_view(), name="lou-status-report-download-slash"),
+    path("follow-up-report", FollowUpReportAPIView.as_view(), name="follow-up-report"),
+    path("follow-up-report/", FollowUpReportAPIView.as_view(), name="follow-up-report-slash"),
     path("lou-status-report", LouStatusReportAPIView.as_view(), name="lou-status-report"),
     path("lou-status-report/", LouStatusReportAPIView.as_view(), name="lou-status-report-slash"),
 ]
